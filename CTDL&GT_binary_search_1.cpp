54b938f3fbe7c54e6c4d8415925ab6824bbe087e
tranchau120103 <125194193+tranchau120103@users.noreply.github.com>
2023-04-04 13:32:21 +0700

    Create CTDL&GT_binary_search_1.cpp

---
+++ CTDL&GT_binary_search_1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int binarySearch(int arr[], int left, int right, int x) {
+    int middle;
+ 
+    while(left <= right) {
+        middle = (left + right) / 2;
+ 
+        if (arr[middle] == x)
+            return middle;
+ 
+        if (x > arr[middle])
+            left = middle + 1;
+        else
+            right = middle - 1;
+    }
+ 
+    return -1;
+}
+int main() {
+    int arr[] = {15, 20, 25, 30, 31, 44, 66};
+ 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 25;
+     
+    int result = binarySearch(arr, 0, n-1, x);
+ 
+    cout << result;
+}
