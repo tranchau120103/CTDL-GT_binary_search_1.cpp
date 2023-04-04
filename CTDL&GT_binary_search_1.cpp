09f46d632256b70c751371e0ee0fc2d075142406
tranchau120103 <125194193+tranchau120103@users.noreply.github.com>
2023-04-04 13:47:25 +0700

    Update CTDL&GT_binary_search_1.cpp

--- CTDL&GT_binary_search_1.cpp
+++ CTDL&GT_binary_search_1.cpp
@@ -1,32 +1,37 @@
-https://freetuts.net/thuat-toan-tim-kiem-nhi-phan-2634.html
+https://blog.luyencode.net/thuat-toan-tim-kiem-nhi-phan/
 
-#include <bits/stdc++.h>
-using namespace std;
- 
-int binarySearch(int arr[], int left, int right, int x) {
-    int middle;
- 
-    while(left <= right) {
-        middle = (left + right) / 2;
- 
-        if (arr[middle] == x)
-            return middle;
- 
-        if (x > arr[middle])
-            left = middle + 1;
-        else
-            right = middle - 1;
+#include <stdio.h>
+#include <conio.h>
+
+// Hàm tìm kiếm nhị phân sử dụng giải thuật đệ quy
+int binarySearch(int arr[], int l, int r, int x) {
+    if (r >= l) {
+        int mid = l + (r - l) / 2; // Tương đương (l+r)/2 nhưng ưu điểm tránh tràn số khi l,r lớn
+
+        // Nếu arr[mid] = x, trả về chỉ số và kết thúc.
+        if (arr[mid] == x)
+            return mid;
+
+        // Nếu arr[mid] > x, thực hiện tìm kiếm nửa trái của mảng
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+
+        // Nếu arr[mid] < x, thực hiện tìm kiếm nửa phải của mảng
+        return binarySearch(arr, mid + 1, r, x);
     }
- 
+
+    // Nếu không tìm thấy
     return -1;
 }
-int main() {
-    int arr[] = {15, 20, 25, 30, 31, 44, 66};
- 
+
+int main(void) {
+    int arr[] = { 2, 3, 4, 10, 40 };
     int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 25;
-     
-    int result = binarySearch(arr, 0, n-1, x);
- 
-    cout << result;
+    int x = 10;
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result == -1)
+        printf("%d xuat hien tai chi so %d", x, result);
+    else
+        printf("%d xuat hien tai chi so %d", x, result);
+    return 0;
 }
